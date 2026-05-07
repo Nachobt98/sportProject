13870c8a46c044ef0c8b0d2b72c3ebbff7518bd8
Nachobt98 <118307668+Nachobt98@users.noreply.github.com>
2026-05-07 23:37:08 +0200

    Remove username-based user API client

--- src/api/usersApi.js
+++ src/api/usersApi.js
@@ -1,11 +1,6 @@
 import { apiFetch } from "./client";
 import { assertOkResponse } from "./response";
 
-export async function getUser(userName) {
-  const response = await apiFetch(`/api/user/${userName}`);
-  return assertOkResponse(response, "No se pudo cargar el usuario");
-}
-
 export async function getCurrentUser() {
   const response = await apiFetch("/api/users/me");
   return assertOkResponse(response, "No se pudo cargar el perfil");
