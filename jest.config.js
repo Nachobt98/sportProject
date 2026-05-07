c1e12248b618a572cb4c5999ca040f8c41c96a22
Nachobt98 <118307668+Nachobt98@users.noreply.github.com>
2026-05-07 12:36:17 +0200

    Focus coverage on testable logic layers

--- jest.config.js
+++ jest.config.js
@@ -9,16 +9,15 @@ module.exports = {
     "^.+\\.[jt]sx?$": "babel-jest",
   },
   moduleNameMapper: {
-    "\\.(css|less|sass|scss)$": "identity-obj-proxy",
+    "\\.(css|less|sass|scss)$": "<rootDir>/src/__mocks__/fileMock.js",
     "\\.(jpg|jpeg|png|gif|webp|svg)$": "<rootDir>/src/__mocks__/fileMock.js",
   },
   collectCoverageFrom: [
-    "backend/src/**/*.js",
+    "backend/src/services/**/*.js",
+    "backend/src/middlewares/**/*.js",
+    "backend/src/utils/**/*.js",
     "src/api/**/*.js",
     "src/context/**/*.js",
-    "!backend/src/server.js",
-    "!backend/src/config/database.js",
-    "!backend/src/config/loadEnv.js",
   ],
   coverageReporters: ["text", "lcov"],
 };
