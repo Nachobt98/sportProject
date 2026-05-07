abdc239790ee5714aad2910fb4520afdc08ea139
Nachobt98 <118307668+Nachobt98@users.noreply.github.com>
2026-05-07 23:36:25 +0200

    Remove username-based user controller handler

--- backend/src/controllers/userController.js
+++ backend/src/controllers/userController.js
@@ -1,26 +1,8 @@
-const User = require("../models/User");
 const userService = require("../services/userService");
-const { normalizeString } = require("../utils/strings");
-const { toPublicUser } = require("../utils/users");
 const { logger } = require("../utils/logger");
 
 function sendServiceResult(res, result) {
   return res.status(result.status).json(result.body);
-}
-
-async function getUser(req, res) {
-  const userName = normalizeString(req.params.userName);
-  try {
-    const user = await User.findOne({ userName }).exec();
-    if (!user) {
-      return res.status(404).json({ message: "Usuario no encontrado" });
-    }
-
-    return res.status(200).json(toPublicUser(user));
-  } catch (error) {
-    logger.error("Error al buscar el usuario", error);
-    return res.status(500).json({ message: "Error al buscar el usuario" });
-  }
 }
 
 async function getCurrentUser(req, res) {
@@ -43,4 +25,4 @@ async function updateCurrentUser(req, res) {
   }
 }
 
-module.exports = { getUser, getCurrentUser, updateCurrentUser };
+module.exports = { getCurrentUser, updateCurrentUser };
