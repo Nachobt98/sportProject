f2a36f943fdbc8ab36c9aa9a870e919f8fc4a728
Nachobt98 <118307668+Nachobt98@users.noreply.github.com>
2026-05-07 12:18:03 +0200

    Extract user controller

---
+++ backend/src/controllers/userController.js
@@ -0,0 +1,20 @@
+const User = require("../models/User");
+const { normalizeString } = require("../utils/strings");
+const { toPublicUser } = require("../utils/users");
+
+async function getUser(req, res) {
+  const userName = normalizeString(req.params.userName);
+  try {
+    const user = await User.findOne({ userName }).exec();
+    if (!user) {
+      return res.status(404).json({ message: "Usuario no encontrado" });
+    }
+
+    return res.status(200).json(toPublicUser(user));
+  } catch (error) {
+    console.error(error);
+    return res.status(500).json({ message: "Error al buscar el usuario" });
+  }
+}
+
+module.exports = { getUser };
