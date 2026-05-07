c79e68bdb4bbe84d0f623f4c78f83961b18e5f5e
Nachobt98 <118307668+Nachobt98@users.noreply.github.com>
2026-05-07 15:38:43 +0200

    Add current user profile routes

--- backend/src/routes/userRoutes.js
+++ backend/src/routes/userRoutes.js
@@ -4,6 +4,8 @@ const { authenticateRequest, requireSameUser } = require("../middlewares/authMid
 
 const router = express.Router();
 
+router.get("/users/me", authenticateRequest, userController.getCurrentUser);
+router.patch("/users/me", authenticateRequest, userController.updateCurrentUser);
 router.get("/user/:userName", authenticateRequest, requireSameUser, userController.getUser);
 
 module.exports = router;
