1f868f2f0dac560d395b49ca016edd66e49566b6
Nachobt98 <118307668+Nachobt98@users.noreply.github.com>
2026-05-07 20:55:46 +0200

    Remove username-based event compatibility routes

--- backend/src/routes/eventRoutes.js
+++ backend/src/routes/eventRoutes.js
@@ -1,10 +1,9 @@
 const express = require("express");
 const eventController = require("../controllers/eventController");
-const { authenticateRequest, requireSameUser } = require("../middlewares/authMiddleware");
+const { authenticateRequest } = require("../middlewares/authMiddleware");
 
 const router = express.Router();
 const authenticated = [authenticateRequest];
-const sameUser = [authenticateRequest, requireSameUser];
 
 router.get("/events", ...authenticated, eventController.listEvents);
 router.post("/events", ...authenticated, eventController.createEvent);
@@ -16,8 +15,5 @@ router.delete("/events/:eventId/join", ...authenticated, eventController.cancelE
 
 router.get("/users/me/events", ...authenticated, eventController.listCurrentUserEvents);
 router.get("/users/me/joined-events", ...authenticated, eventController.listCurrentUserJoinedEvents);
-
-router.get("/user/:userName/events", ...sameUser, eventController.listUserEvents);
-router.get("/user/:userName/joinedEvents", ...sameUser, eventController.listJoinedEvents);
 
 module.exports = router;
