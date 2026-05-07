bb16fd57e3960315b6dc9aa73b207348d84bb96b
Nachobt98 <118307668+Nachobt98@users.noreply.github.com>
2026-05-07 23:35:49 +0200

    Remove username-based user compatibility route

--- backend/src/routes/userRoutes.js
+++ backend/src/routes/userRoutes.js
@@ -1,11 +1,10 @@
 const express = require("express");
 const userController = require("../controllers/userController");
-const { authenticateRequest, requireSameUser } = require("../middlewares/authMiddleware");
+const { authenticateRequest } = require("../middlewares/authMiddleware");
 
 const router = express.Router();
 
 router.get("/users/me", authenticateRequest, userController.getCurrentUser);
 router.patch("/users/me", authenticateRequest, userController.updateCurrentUser);
-router.get("/user/:userName", authenticateRequest, requireSameUser, userController.getUser);
 
 module.exports = router;
