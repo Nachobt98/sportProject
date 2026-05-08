b7b2c0ab30cd547d4e2fcff6d0fdd38f8104d1b6
Nachobt98 <118307668+Nachobt98@users.noreply.github.com>
2026-05-08 09:18:36 +0200

    Split Jest into frontend and backend projects

--- jest.config.js
+++ jest.config.js
@@ -1,29 +1,45 @@
+const coveragePatterns = [
+  "backend/src/**/*.js",
+  "src/**/*.js",
+  "!backend/src/app.js",
+  "!backend/src/config/**",
+  "!backend/src/models/**",
+  "!backend/src/routes/**",
+  "!backend/src/server.js",
+  "!src/__mocks__/**",
+  "!src/index.js",
+  "!src/reportWebVitals.js",
+  "!src/setupTests.js",
+];
+
+const moduleNameMapper = {
+  "\\.(css|less|sass|scss)$": "<rootDir>/src/__mocks__/fileMock.js",
+  "\\.(jpg|jpeg|png|gif|webp|svg)$": "<rootDir>/src/__mocks__/fileMock.js",
+};
+
 module.exports = {
-  testEnvironment: "jsdom",
-  setupFilesAfterEnv: ["<rootDir>/src/setupTests.js"],
-  testMatch: [
-    "<rootDir>/src/**/*.test.js",
-    "<rootDir>/backend/src/**/*.test.js",
-  ],
-  transform: {
-    "^.+\\.[jt]sx?$": "babel-jest",
-  },
-  moduleNameMapper: {
-    "\\.(css|less|sass|scss)$": "<rootDir>/src/__mocks__/fileMock.js",
-    "\\.(jpg|jpeg|png|gif|webp|svg)$": "<rootDir>/src/__mocks__/fileMock.js",
-  },
-  collectCoverageFrom: [
-    "backend/src/**/*.js",
-    "src/**/*.js",
-    "!backend/src/app.js",
-    "!backend/src/config/**",
-    "!backend/src/models/**",
-    "!backend/src/routes/**",
-    "!backend/src/server.js",
-    "!src/__mocks__/**",
-    "!src/index.js",
-    "!src/reportWebVitals.js",
-    "!src/setupTests.js",
-  ],
+  collectCoverageFrom: coveragePatterns,
   coverageReporters: ["text", "lcov"],
+  projects: [
+    {
+      displayName: "frontend",
+      testEnvironment: "jsdom",
+      setupFilesAfterEnv: ["<rootDir>/src/setupTests.js"],
+      testMatch: ["<rootDir>/src/**/*.test.js"],
+      transform: {
+        "^.+\\.[jt]sx?$": "babel-jest",
+      },
+      moduleNameMapper,
+    },
+    {
+      displayName: "backend",
+      testEnvironment: "node",
+      setupFilesAfterEnv: ["<rootDir>/src/setupTests.js"],
+      testMatch: ["<rootDir>/backend/src/**/*.test.js"],
+      transform: {
+        "^.+\\.[jt]sx?$": "babel-jest",
+      },
+      moduleNameMapper,
+    },
+  ],
 };
