a3cade67c0ec9fbf348b5c5d054b81e9987c3bd7
Nachobt98 <118307668+Nachobt98@users.noreply.github.com>
2026-05-08 10:34:12 +0200

    Include JSX source files in Jest coverage

--- jest.config.js
+++ jest.config.js
@@ -1,13 +1,14 @@
 const coveragePatterns = [
   "backend/src/**/*.js",
   "src/**/*.js",
+  "src/**/*.jsx",
   "!backend/src/app.js",
   "!backend/src/config/**",
   "!backend/src/models/**",
   "!backend/src/routes/**",
   "!backend/src/server.js",
   "!src/__mocks__/**",
-  "!src/index.js",
+  "!src/index.jsx",
   "!src/reportWebVitals.js",
   "!src/setupTests.js",
 ];
@@ -15,6 +16,10 @@ const coveragePatterns = [
 const moduleNameMapper = {
   "\\.(css|less|sass|scss)$": "<rootDir>/src/__mocks__/fileMock.js",
   "\\.(jpg|jpeg|png|gif|webp|svg)$": "<rootDir>/src/__mocks__/fileMock.js",
+};
+
+const transform = {
+  "^.+\\.[jt]sx?$": "babel-jest",
 };
 
 module.exports = {
@@ -26,9 +31,8 @@ module.exports = {
       testEnvironment: "jsdom",
       setupFilesAfterEnv: ["<rootDir>/src/setupTests.js"],
       testMatch: ["<rootDir>/src/**/*.test.js"],
-      transform: {
-        "^.+\\.[jt]sx?$": "babel-jest",
-      },
+      transform,
+      moduleFileExtensions: ["jsx", "js", "json", "node"],
       moduleNameMapper,
     },
     {
@@ -36,9 +40,8 @@ module.exports = {
       testEnvironment: "node",
       setupFilesAfterEnv: ["<rootDir>/src/setupTests.js"],
       testMatch: ["<rootDir>/backend/src/**/*.test.js"],
-      transform: {
-        "^.+\\.[jt]sx?$": "babel-jest",
-      },
+      transform,
+      moduleFileExtensions: ["js", "json", "node"],
       moduleNameMapper,
     },
   ],
