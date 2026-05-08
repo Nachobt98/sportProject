8bbdfd80d19da3475fe2d8cc52eb633279a8d274
Nachobt98 <118307668+Nachobt98@users.noreply.github.com>
2026-05-08 09:15:47 +0200

    Replace CRA Babel preset with explicit Jest config

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,6 @@
 module.exports = {
-  presets: ["react-app"],
+  presets: [
+    ["@babel/preset-env", { targets: { node: "current" } }],
+    ["@babel/preset-react", { runtime: "automatic" }],
+  ],
 };
