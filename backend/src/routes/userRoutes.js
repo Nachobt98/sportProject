82b512e2beb657c8027d0a61a79792f1f1dbbf92
Nachobt98 <118307668+Nachobt98@users.noreply.github.com>
2026-05-09 12:28:48 +0200

    Add profile image upload route

--- backend/src/routes/userRoutes.js
+++ backend/src/routes/userRoutes.js
@@ -1,10 +1,12 @@
 const express = require("express");
 const userController = require("../controllers/userController");
 const { authenticateRequest } = require("../middlewares/authMiddleware");
+const { handleProfileImageUpload } = require("../middlewares/profileImageUploadMiddleware");
 
 const router = express.Router();
 
 router.get("/users/me", authenticateRequest, userController.getCurrentUser);
 router.patch("/users/me", authenticateRequest, userController.updateCurrentUser);
+router.post("/users/me/profile-image", authenticateRequest, handleProfileImageUpload, userController.updateCurrentUserProfileImage);
 
 module.exports = router;
